ba0b9665c06391ff16142e3da2ff0e2cb4e5161d
Herman Griffin <97112+frek818@users.noreply.github.com>
2022-12-07 05:19:51 -0500

    day 7 initial

---
+++ solutions/2022/07/python/solution.py
@@ -0,0 +1,23 @@
+"""
+# REPLACE WITH TITLE
+"""
+from aocd.models import Puzzle
+
+
+def solution_1(input_data: str):
+    "part 1"
+    return 0
+
+
+def solution_2(input_data: str):
+    "part 2"
+    return 0
+
+
+year, day = 2022, 7
+assert year and day, "year and day must be set"
+
+if __name__ == '__main__':
+    puzzle = Puzzle(year=year, day=day)
+    print(solution_1(puzzle.input_data))
+    print(solution_2(puzzle.input_data))

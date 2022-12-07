c9146b8155620676bf0adcd6b377378a243c0348
Herman Griffin <97112+frek818@users.noreply.github.com>
2022-12-07 05:25:04 -0500

    rstrip to lstrip

--- template/python/test_solution.py
+++ template/python/test_solution.py
@@ -6,30 +6,7 @@ from .solution import solution_1, solution_2
 @pytest.fixture
 def input_data():
     return """
-$ cd /
-$ ls
-dir a
-14848514 b.txt
-8504156 c.dat
-dir d
-$ cd a
-$ ls
-dir e
-29116 f
-2557 g
-62596 h.lst
-$ cd e
-$ ls
-584 i
-$ cd ..
-$ cd ..
-$ cd d
-$ ls
-4060174 j
-8033020 d.log
-5626152 d.ext
-7214296 k
-""".rstrip("\n")
+""".lstrip("\n")
 
 def test_solution_1(input_data):
     print(input_data)

30dfd37dd0d91a065d8908909db0262a970e752e
frek818 <97112+frek818@users.noreply.github.com>
2022-12-10 01:17:21 -0500

    cleaned (#15)

--- solutions/2022/09/python/solution.py
+++ solutions/2022/09/python/solution.py
@@ -1,19 +1,15 @@
 """
 --- Day 9: Rope Bridge ---
 """
+from functools import reduce
 from typing import List, Tuple
 from aocd.models import Puzzle
 
 
 class Knot:
-    def __init__(self, x, y, id: int):
+    def __init__(self, x, y):
         self._x = x
         self._y = y
-        self._id = id
-
-    @property
-    def id(self) -> str:
-        return str(self._id)
 
     def move(self, direction):
         match direction:
@@ -39,98 +35,45 @@ def parse_moves(input_data: str):
     ]
 
 
-def create_knots(amount):
-    return [Knot(0, 0, idx) for idx in range(amount)]
+def move_a(knot: Knot, move: str) -> Knot:
+    knot.move(move)
+    return knot
 
 
-def control_head(knot: Knot, direction):
-    knot.move(direction)
-    return knot.position()
-
-
-def control_tail_(knot: Knot, following_position):
-    bx, by = knot.position()
-    ax, ay = following_position
-    if abs(ay - by) == 2:
-        if ay > by:
-            knot.move("U")
-        else:
-            knot.move("D")
-    if abs(ax - bx) == 2:
-        if ax > bx:
-            knot.move("R")
-        else:
-            knot.move("L")
-    return knot.position()
-
-
-def control_tail(knot: Knot, following_position):
-    bx, by = knot.position()
-    ax, ay = following_position
-
-    # diagonal procedure
+def move_b(knot_h: Knot, knot_t: Knot) -> Knot:
+    bx, by = knot_t.position()
+    ax, ay = knot_h.position()
+    if not (abs(ay - by) == 2 or abs(ax - bx) == 2):
+        return knot_t
     if ax != bx and ay != by:
-        if not (abs(ay - by) == 2 or abs(ax - bx) == 2):
-            return
-
-        if ax > bx:
-            knot.move("R")
-        else:
-            knot.move("L")
-
-        if ay > by:
-            knot.move("U")
-        else:
-            knot.move("D")
-
-    # horizontal procedure
+        knot_t.move("U") if ay > by else knot_t.move("D")
+        knot_t.move("R") if ax > bx else knot_t.move("L")
     elif ax == bx:
-        if abs(ay - by) == 2:
-            if ay > by:
-                knot.move("U")
-            else:
-                knot.move("D")
-
-    # vertical procedure
+        knot_t.move("U") if ay > by else knot_t.move("D")
     elif ay == by:
-        if abs(ax - bx) == 2:
-            if ax > bx:
-                knot.move("R")
-            else:
-                knot.move("L")
-
-    return knot.position()
+        knot_t.move("R") if ax > bx else knot_t.move("L")
+    return knot_t
 
 
-def update_positions(knots: List[Knot], head_move):
-    control_head(knots[0], head_move)
-    for tail_index in range(1, len(knots)):
-        control_tail(knots[tail_index], knots[tail_index - 1].position())
-
-
-def knot_visited_locations(
-    knots: List[Knot], moves: List[Tuple[str, int]], knot_idx=-1
-):
-    visited_locations = set()
-    for direction, steps in moves:
-        for _ in range(steps):
-            update_positions(knots, direction)
-            visited_locations.add(knots[knot_idx].position())
-    return visited_locations
+def tail_visited_count(knots: List[Knot], moves: Tuple[str, int]) -> int:
+    return len(set([reduce(move_b, knots[1:], move_a(knots[0], direction)).position()
+                    for direction, steps in moves
+                    for _ in range(steps)
+                    ]))
 
 
 def solution_1(input_data: str):
     "part 1"
-    knots = create_knots(2)
+    knots = [Knot(0, 0) for _ in range(2)]
     moves = parse_moves(input_data)
-    return len(knot_visited_locations(knots, moves))
+    return tail_visited_count(knots, moves)
 
 
 def solution_2(input_data: str):
     "part 2"
-    knots = create_knots(10)
+    knots = [Knot(0, 0) for _ in range(10)]
     moves = parse_moves(input_data)
-    return len(knot_visited_locations(knots, moves))
+    return tail_visited_count(knots, moves)
 
 
 year, day = 2022, 9
